c54978f33a3f16688dea20fcc2ec514fb923158e
Jakub Černý <kubiik.c@seznam.cz>
2023-06-04 21:58:14 +0200

    FINAL

--- login.php
+++ login.php
@@ -13,17 +13,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        echo "Username already taken";
+        session_start();
+        $_SESSION["username"] = $username;
+        header("Location: index.html"); 
+        exit();
     } else {
-        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-        if ($conn->query($sql) === TRUE) {
-			header("Location: pages/login.html");
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        echo "Invalid username or password";
     }
 }
 $conn->close();
